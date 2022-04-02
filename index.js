d95c955f532797dfc00554f6e1c3c64cb6d68628
Douglas Horstmann <66478601+douglash93@users.noreply.github.com>
2022-04-02 19:25:32 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const btnGerarCoronavac = document.querySelector('#gerar-coronavac');
+const btnFecharCoronavac = document.querySelector('#fechar-coronavac');
+const coronavacBg = document.querySelectorAll('.coronavac-bg')[0];
+const coronavac = document.querySelectorAll('.coronavac')[0];
+const switchMaizena = document.querySelector('#flexSwitchCheckMaizena');
+const itens = document.querySelector('#itens');
+const selectJs = document.querySelector('#js');
+
+const imagens = [
+    'angular.png',
+    'js.png',
+    'react.png',
+    'vue.jpg',
+];
+
+btnGerarCoronavac.addEventListener('click', () => {
+    coronavac.classList.toggle('open');
+    coronavacBg.classList.toggle('open');
+});
+
+btnFecharCoronavac.addEventListener('click', () => {
+    coronavac.classList.toggle('open');
+    coronavacBg.classList.toggle('open');
+});
+
+switchMaizena.addEventListener('change', (e) => {
+
+    const isChecked = e.target.checked
+    if(isChecked) {
+        itens.classList.add('row-cols-md-3');
+        itens.classList.remove('row-cols-md-2');
+        itens.children[2].classList.remove('d-none');
+        itens.children[2].classList.add('d-block');
+        return;
+    }
+
+    itens.classList.add('row-cols-md-2');
+    itens.classList.remove('row-cols-md-3');
+    itens.children[2].classList.remove('d-block');
+    itens.children[2].classList.add('d-none');
+});
+
+selectJs.addEventListener('change', (e) => {
+    let imagem = imagens.find(x => x.includes(e.target.value));
+    document.querySelector('#imgBgJs').src = `/img/${imagem}`;
+    setTimeout(function() {
+        coronavac.classList.toggle('open');
+        coronavacBg.classList.toggle('open');
+    }, 1100);
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+# JavaScript e Maizena cria Coronavac?
+
+ Projeto baseado em uma pesquisa no Google Trends, por alguma razão na época da pesquisa, era um assunto que estava com uma grande ascenção. 
+
+ E é o que acontece, quando um dev fica com um pouquinho de tempo livre aos sábados. 
+
+ [Bora se conectar]
+ https://www.linkedin.com/in/douglas-ahorstman/
+ 
+ [Se inscreve lá pra ficar por dentro de mais conteúdos maneiros de JS]
+ https://www.youtube.com/channel/UCP9C5I2j-MePwtwvIIaAsTQ
+
+![Google Treends](img/google-trends.PNG)
